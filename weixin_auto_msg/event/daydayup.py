6674a3d0d46458c87d02699f15615efeaa0dc031
alenfive <kobe96688@126.com>
2016-08-21 11:03:53 +0800

    Initial commit

--- weixin_auto_msg/event/daydayup.py
+++ weixin_auto_msg/event/daydayup.py
@@ -1,6 +1,5 @@
 
 import requests
-from PIL import Image,ImageTk
 import threading
 import io
 import base64
@@ -8,6 +7,7 @@ import xml.etree.ElementTree as Etree
 import json
 import shutil
 import time
+from datetime import datetime, timedelta
 
 #HTTPClient实例
 class HttpClient:
@@ -16,14 +16,36 @@ class HttpClient:
     def get_http(self):
         return self.http
 
-
-class SyncCheckThread(threading.Thread):
-
+class TimerSendMsg(threading.Thread):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"}
     session = None
 
-    def __init__(self,session):
+    def __init__(self, session):
+        threading.Thread.__init__(self)
         self.session = session
 
+    def run(self):
+        msgcore = MsgCore(self.session)
+        while True:
+            curTime = datetime.now()
+            desTime = curTime.replace(hour=5, minute=20, second=0, microsecond=0)
+            delta = curTime - desTime
+            if abs(delta.total_seconds()) < 60*10:
+                msgcore.sendmsg(Webwxinit.jsonData['User'], msgcore.finduserByNickname("郑小小"), "早起的虫子开始吃鸟了")
+                time.sleep(60*10*2)
+                continue
+
+            time.sleep(60)
+
+class SyncCheckThread(threading.Thread):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"}
+    session = None
+
+    def __init__(self, session):
+        threading.Thread.__init__(self)
+        self.session = session
 
     def run(self):
         print("start sync check ...")
@@ -40,19 +62,117 @@ class SyncCheckThread(threading.Thread):
         #C1001_1471480412 % 7
         #C1004_1471314833 & _ = 1471491460322
 
-        #while True:
-         #url = "https://webpush.wx2.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=1471491463975&skey=" + TicketInfo.skey + "&sid="+TicketInfo.wxsid+"&uin="+TicketInfo.wxuin
+        #self.sendmsg(Webwxinit.jsonData['User'],self.finduserByNickname("潘建兴"),"在做啥子")
+        msgcore = MsgCore(self.session)
+        while True:
+            url = "https://webpush.wx2.qq.com/cgi-bin/mmwebwx-bin/synccheck?r=1471491463975&skey="+TicketInfo.skey+"&sid="+TicketInfo.wxsid+"&uin="+TicketInfo.wxuin+"&synckey="
+            for item in Webwxinit.jsonData['SyncKey']['List']:
+                url = url + ""+str(item['Key'])+"_"+str(item['Val'])+"|"
+            print("GET " + url)
+            r = self.session.get(url, headers=self.headers, allow_redirects=False,timeout=60)
+            r.encoding = "utf-8"
+            print(r.text)
+            if r.text.find("retcode:\"0\"") == -1:
+                break
+
+            if r.text.find("selector:\"0\"") != -1:
+                continue
+
+            #https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid=VznnR+RhwgnLTued&skey=@crypt_fd882096_f130cf0ca102faafe158aecd5b4b3fa8
+            syncUrl = "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxsync?sid="+TicketInfo.wxsid+"&skey="+TicketInfo.skey
+            print("POST request", syncUrl)
+
+            headers = {"Content-type": "application/json;charset=UTF-8"}
+            headers.update(self.headers)
+            params = {"BaseRequest": {"Uin": TicketInfo.wxuin, "Sid": TicketInfo.wxsid,"Skey":TicketInfo.skey},"SyncKey":Webwxinit.jsonData['SyncKey']}
+            print(params)
+            syncResult = self.session.post(syncUrl, headers=headers, data=json.JSONEncoder().encode(params),timeout=60)
+            syncResult.encoding = "utf-8"
+            print("result", syncResult.text)
+            Webwxinit.jsonData['SyncKey'] = syncResult.json()['SyncKey']
+
+            sendUserList = []
+            for msg in syncResult.json()['AddMsgList']:
+                if msg['FromUserName'] == Webwxinit.jsonData['User']['UserName']:
+                    continue
+                if msg['ToUserName'] != Webwxinit.jsonData['User']['UserName']:
+                    continue
+                # if msg['MsgType'] != 1:
+                #     continue
+
+                fromUser = msgcore.finduserByUsername(msg['FromUserName'])
+
+                if fromUser is None:
+                    continue
+
+                print("receive from",fromUser['NickName']," Content:",msg['Content'])
+                sendUserList.append(fromUser)
+                self.appenduser(sendUserList,fromUser)
 
 
+            for fu in sendUserList:
+                msgcore.sendmsg(Webwxinit.jsonData['User'], fu, "我二爷又去加班了 别找他")
 
+            time.sleep(5)
+
+        print("已在其他地方登录。断开连接...")
+
+    def appenduser(self,userList,user):
+        for item in userList:
+            if item['UserName'] == user['UserName']:
+                return
+        userList.append(user)
+
+
+class MsgCore:
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"}
+    session = None
+
+    def __init__(self, session):
+        self.session = session
+
+    def sendmsg(self, fromusername,tousername, msg):
+        if fromusername is None or tousername is None:
+            return
+
+        print("send msg ",tousername['NickName']," content",msg)
+        sendUrl = "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxsendmsg?lang=zh_CN&pass_ticket="+TicketInfo.pass_ticket
+        print("POST request", sendUrl)
+        headers = {"Content-type": "application/json;charset=UTF-8"}
+        headers.update(self.headers)
+        params = {"BaseRequest": {"Uin": TicketInfo.wxuin, "Sid": TicketInfo.wxsid, "Skey": TicketInfo.skey},
+                  "Msg": {"Content":msg,"FromUserName":fromusername['UserName'],"ToUserName":tousername['UserName'],"Type":1},
+                  "Scene":0}
+        print(params)
+        sendResult = self.session.post(sendUrl, headers=headers, data=json.dumps(params,ensure_ascii=False).encode("utf-8"),timeout=60)
+        sendResult.encoding = "utf-8"
+        print("result", sendResult.text)
+
+    def finduserByUsername(self,username):
+        if Webwxinit.jsonData['User']['UserName'] == username:
+            return Webwxinit.jsonData['User']
+
+        for item in Concat.jsonData['MemberList']:
+            if item['UserName'] == username:
+                return item
+        return None
+
+    def finduserByNickname(self,nickName):
+
+        if Webwxinit.jsonData['User']['UserName'] == nickName or Webwxinit.jsonData['User']['RemarkName'] == nickName:
+            return Webwxinit.jsonData['User']
+
+        for item in Concat.jsonData['MemberList']:
+            if item['NickName'] == nickName or item['RemarkName'] == nickName:
+                return item
+        return None
 
 class CheckScanThread(threading.Thread):
 
-    def __init__(self,qrcode,label,state):
+    def __init__(self, qrcode):
         threading.Thread.__init__(self)
         self.qrcode = qrcode
-        self.label = label
-        self.state = state
 
     def run(self):
         print("scan...")
@@ -60,23 +180,8 @@ class CheckScanThread(threading.Thread):
             data = self.qrcode.checkScan()
             if data == 'window.code=408;':
                 continue
-
-            headStr = data.split("\'")[1].replace("data:img/jpg;base64,","")
-
-            imgdata = base64.b64decode(headStr)
-            file = open('/home/freedom/head.jpg', 'wb')
-            file.write(imgdata)
-            file.flush()
-            file.close()
-
-            img = Image.open('/home/freedom/head.jpg',mode="r")
-            background_image = ImageTk.PhotoImage(img)
-
-            self.label.config(image=background_image)
-            self.label.image = background_image
             break
 
-        self.state.config(text="扫描成功，请确认")
         print("confim...")
         dothing = Dothing(self.qrcode.session)
         ticket_url = None
@@ -90,11 +195,9 @@ class CheckScanThread(threading.Thread):
             ticket_url = arr[1]
             break
 
-        self.state.config(text="已确认，正在验证ticket")
+        print("已确认，正在验证ticket")
         dothing.checkTicket(ticket_url)
-
-        self.state.config(text="认证成功,获取用户数据")
-
+        print("认证成功,获取用户数据")
         dothing.webwxinit()
         dothing.getConcat()
 
@@ -161,6 +264,18 @@ class Dothing:
         #print(r.json()['MemberList'][0]['NickName'])
         print(r.text)
 
+    # def webwxbatchgetcontact(self):
+    #     url = "https://wx2.qq.com/cgi-bin/mmwebwx-bin/webwxbatchgetcontact?type=ex&r=1471693769173"
+    #     print("POST request", url)
+    #
+    #     headers = {"Content-type": "application/json;charset=UTF-8"}
+    #     headers.update(self.headers)
+    #     params = {"BaseRequest": {"Uin": TicketInfo.wxuin, "Sid": TicketInfo.wxsid}}
+    #     print(params)
+    #     r = self.session.post(url, headers=headers, data=json.JSONEncoder().encode(params))
+    #     r.encoding = "utf-8"
+    #     Webwxinit.jsonData = r.json()
+    #     print("result", r.text)
 
 #验证码
 class QRCode:
@@ -186,7 +301,7 @@ class QRCode:
         url = "https://login.wx2.qq.com/qrcode/"+uuid
         print("GET request :", url)
         r = self.session.get(url,headers = self.headers,stream = True)
-        path = "/home/freedom/qcode.jpg"
+        path = "/qcode.jpg"
         with open(path,'wb') as f:
             r.raw.decode_content = True
             shutil.copyfileobj(r.raw,f)
@@ -208,25 +323,32 @@ class QRCode:
         return r.text
 
 
-def start(label,state):
-
-
+def start():
     session = requests.session()
     qrcode = QRCode(session)
     path = qrcode.getCodeImg()
-    img = Image.open(path,mode="r")
-    background_image = ImageTk.PhotoImage(img)
 
-    label.config(image=background_image)
-    label.image = background_image
-
-    state.config(text="等待手机扫描中...")
-
-    check = CheckScanThread(qrcode,label,state)
-    check.setDaemon(label)
+    check = CheckScanThread(qrcode)
     check.start()
     print("start...")
     check.join()
-
     print("SyncCheckThread ..")
-    SyncCheckThread(session).start()
+
+    TimerSendMsg(session).start()
+
+    while True:
+        try:
+            sct = SyncCheckThread(session)
+            sct.start()
+            sct.join()
+        except Exception as e:
+            print(e)
+
+
+
+
+
+
+if __name__ == '__main__':
+    start()
+    #SyncCheckThread(requests.session()).run()
